d0048f05f781b6b128e9e4d5a20e508f33f335fb
Annisa'U Rahma <annisau.rahma@binus.ac.id>
2024-04-08 21:11:57 +0700

    initial commit

---
+++ routes/web.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use Laravel\Socialite\Facades\Socialite;
+use App\Http\Controllers\AuthController;
+use Illuminate\Support\Facades\Auth;
+
+ 
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::redirect('home', 'dashboard');
+
+Route::get('/auth', [AuthController::class, 'index'])->name('login')->middleware('guest');
+
+Route::get('/auth/redirect', [AuthController::class, 'redirect'])->middleware('guest');
+ 
+Route::get('/auth/callback', [AuthController::class, 'callback'])->middleware('guest');
+
+Route::get('/auth/logout', [AuthController::class, 'logout']);
+
+Route::get('/dashboard', function(){
+    return view('dashboard/index');
+})->middleware('auth');
